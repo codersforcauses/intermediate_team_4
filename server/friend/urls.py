c9a2b797c6888cfbf830f3f600738a8e5af76ba1
Kush Patel <kushpatel.personal@gmail.com>
2026-01-07 23:18:43 +0800

    Updated urls for friends for managing and viewing friendships and friendship requests

---
+++ server/friend/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("send/<int:user_id>/", views.SendFriendRequest.as_view()),
+    path("accept/<int:request_id>/", views.AcceptFriendRequest.as_view()),
+    path("decline/<int:request_id>/", views.DeclineFriendRequest.as_view()),
+    path("cancel/<int:request_id>/", views.CancelFriendRequest.as_view()),
+    path("requests_sent/", views.MySentRequests.as_view()),
+    path("requests/", views.MyFriendRequests.as_view()),
+    path("list/", views.MyFriends.as_view()),
+    path("remove/<int:user_id>/", views.RemoveFriends.as_view()), # added here
+]
