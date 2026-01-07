c7aaca1b2a72852a3bd9b41d969c1e1798d6531f
Kush Patel <kushpatel.personal@gmail.com>
2026-01-07 23:09:03 +0800

    Login and Logout redirect urls set in settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -169,3 +169,6 @@ STATICFILES_DIRS = ("static",)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/api/user/"
+LOGOUT_REDIRECT_URL = "/api-auth/login/"
