1319390cd725c1c8909dd770cdd4838ae26104ad
Kush Patel <kushpatel.personal@gmail.com>
2026-01-08 21:34:21 +0800

    added me url for user_profile

--- server/user_profile/urls.py
+++ server/user_profile/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = "user_profile"
 urlpatterns = [ 
+    path("me/", views.Me.as_view(), name="me"),
     path("profile/<int:pk>/", views.UserProfileDetail.as_view(), name="profile-detail"), 
     path("profile/", views.UserProfileList.as_view(), name="profile-list"), 
     path("", views.UserList.as_view(), name="user-list"), 
+
 ]
