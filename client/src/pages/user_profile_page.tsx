7b0a29937dae13e5666e9ebdd01dc0c1947975f6
Kush Patel <kushpatel.personal@gmail.com>
2026-01-09 13:53:16 +0800

    Comment notes on how to set profile view of current logged in user vs profile view of selected user

--- client/src/pages/user_profile_page.tsx
+++ client/src/pages/user_profile_page.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 import User_Navbar from "../components/ui/user_navbar";
 import { useAuth } from "../context/AuthContext";
@@ -68,6 +68,10 @@ export default function UserPage() {
           {/* User details */}
           <h2 style={{ marginTop: "25px", fontSize: "28px" }}>
             {user?.username}
+
+            {/* Should instead be an api call for friends name of curent user profile viewed, 
+              not necessarily the current logged in user, update later in backend and frontend, 
+              (could update existing views with permissions of auth allowing interactivity ?? ) */}
           </h2>
           <p style={{ color: "#666", marginBottom: "40px", fontSize: "18px" }}>
             Perth, Western Australia
@@ -85,9 +89,21 @@ export default function UserPage() {
             <Link href="./user_friends">
               <Stat label="Friends" value={friends.length} />
             </Link>
+
+            {/* Should instead be an api call for friends number of curent user profile viewed, 
+            not necessarily the current logged in user, update later in backend and frontend, 
+            and link should also point to specific page for friends of user being viewed 
+            (could update existing views with permissions of auth allowing interactivity ?? ) */}
+
             <Stat label="Mutual friends" value={0} />
             <Stat label="Clubs joined" value={0} />
           </div>
+          {/* { user === profile_user (
+
+            ) : (
+              <Stat label="Mutual friends" value={0} />
+            )
+            } */}
         </div>
       </div>
     </>
