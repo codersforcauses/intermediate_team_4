364f4cb5791c6f932c8d4f4f2146d64e7a1d6229
sylee212 <singyuan212@gmail.com>
2026-01-09 22:14:13 +0800

    fixed inventory

--- client/src/pages/organization_whole_inventory.tsx
+++ client/src/pages/organization_whole_inventory.tsx
@@ -10,8 +10,10 @@ import { useOrganizationBackendGetItems } from "../components/ui/backend/organiz
 
 const Organization_Whole_Inventory = () => {
   // 1. Fetch the data using your custom hook
-  const { data, loading, error, refresh } =
-    useOrganizationBackendGetItems("all");
+  const { data, loading, error, refresh } = useOrganizationBackendGetItems(
+    "",
+    "",
+  );
   console.log("Refresh function:", refresh);
 
   // for the overlay model state management
